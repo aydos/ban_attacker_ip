48e6c38b9fee7d706b9007b4f88fbe5ca77fef93
aydos <fahriaydos@yahoo.com>
2015-05-29 09:12:34 +0300

    Update banip.sh

--- banip.sh
+++ banip.sh
@@ -6,15 +6,15 @@
 # NOT TESTED YET
 
 # iplist dosyası yoksa oluşturur
-touch iplist
+touch /tmp/iplist
 
 # bir tane olanları iptables/firewall'dan çıkarır
 # birden fazla olanlar iptables/firewall'da kalmaya devam eder
 #sort iplist | uniq -c | awk '{if($1==1) print $2}' | xargs -I ip iptables -D INPUT -s ip -j DROP
-sort iplist | uniq -c | awk '{if($1==1) print $2}' | xargs -I IP firewall-cmd --zone=public --remove-rich-rule='rule family="ipv4" source address="IP" reject'
+sort /tmp/iplist | uniq -c | awk '{if($1==1) print $2}' | xargs -I IP firewall-cmd --zone=public --remove-rich-rule='rule family="ipv4" source address="IP" reject'
 
 # her ip'den bir tanesini siler
-sort iplist -u | xargs -I ip sed -i '0,/'ip'/{//d;}' iplist
+sort /tmp/iplist -u | xargs -I ip sed -i '0,/'ip'/{//d;}' iplist
 
 # son 5 dakika içinde yapılan atakları dosyaya yazar
 # root'a yapılan ataklar (root login'i kapatılmalı)
@@ -24,10 +24,10 @@ sed -n "/^$(date --date='5 minutes ago' '+%b %d %H:')/,\$p" /var/log/secure | gr
 
 # yeni gelen ip'leri iptables/firewall'a ekler
 #sort newips -u | xargs -I ip iptables -A INPUT -s ip -j DROP
-sort newips -u | xargs -I IP firewall-cmd --zone=public --add-rich-rule='rule family="ipv4" source address="IP" reject'
+sort /tmp/newips -u | xargs -I IP firewall-cmd --zone=public --add-rich-rule='rule family="ipv4" source address="IP" reject'
 
 # yeni ip'leri listeye ekler
-cat newips >> iplist
+cat /tmp/newips >> /tmp/iplist
 #rm -rf newips
 
 # iptables'ı kaydeder
